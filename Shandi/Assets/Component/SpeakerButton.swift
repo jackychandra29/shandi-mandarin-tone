3f85766510e3ace2dfdbad3bf5f9689596f78b69
Garry Agassi <agassi.garry@gmail.com>
2026-06-14 08:00:19 +0700

    fix button speaker shadow

--- Shandi/Assets/Component/SpeakerButton.swift
+++ Shandi/Assets/Component/SpeakerButton.swift
@@ -5,9 +5,9 @@ struct SpeakerButton: View {
 
     var body: some View {
         ZStack {
-            Circle()
-                .fill(Color.yellowShadow)
-                .offset(y: 4)
+//            Circle()
+//                .fill(Color.yellowShadow)
+//                .offset(y: 4)
 
             Button(action: action) {
                 Image(systemName: "speaker.wave.2.fill")
@@ -18,6 +18,12 @@ struct SpeakerButton: View {
                     .clipShape(Circle())
             }
             .buttonStyle(.plain)
+            .shadow(
+                color: Color.yellowShadow,
+                radius: 0,
+                x: 0,
+                y: 4
+            )
         }
         .frame(width: 40, height: 40)
     }
